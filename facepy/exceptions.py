44fe82900ec172ad52b094840cb3adb3a489bb06
Johannes <jgorset@gmail.com>
2011-04-20 22:53:35 +0200

    Remove APIUnavailableError

--- facepy/exceptions.py
+++ facepy/exceptions.py
@@ -1,6 +1,5 @@
 class FacepyError(Exception):
     """Base class for exceptions raised by Facepy."""
     pass
-    
-class APIUnavailableError(FacepyError): pass
+
 class APIError(FacepyError): pass
