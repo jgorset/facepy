a01d39e03b249feb6d7bcd325e461ba1ddb76dae
Johannes Gorset <jgorset@gmail.com>
2011-05-10 15:55:14 +0200

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'facepy',
-  version = '0.2',
+  version = '0.2.1',
   description = "Facepy is an API client for Facebook's Graph API that doesn't suck.",
   author = "Johannes Gorset",
   author_email = "jgorset@gmail.com",
