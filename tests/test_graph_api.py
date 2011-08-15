77ab1f7b68dd8bda7e63edde2291799917aba83f
Johannes Gorset <jgorset@gmail.com>
2011-08-15 15:45:27 +0200

    Add signed request and application secret to tests

--- tests/test_graph_api.py
+++ tests/test_graph_api.py
@@ -9,8 +9,13 @@ import requests
 # Permanent access token for a test user of the Facepy Test Application (application id 160327664029652)
 TEST_ACCESS_TOKEN = '160327664029652|71c2bc094b012a6f7f37ece4.0-100002387854815|gmGJg6Sc2nBzssDrCXL_w49bDO8'
 
-TEST_SIGNED_REQUEST = ""
-TEST_APP_SECRET = ""
+TEST_SIGNED_REQUEST = '7MF856LgfXmXf0PPe4BOWq20FVVZQLAebjlWAh2e64k.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4' \
+                      'cGlyZXMiOjAsImlzc3VlZF9hdCI6MTMxMzQxNDQ1MCwib2F1dGhfdG9rZW4iOiIxNjAzMjc2NjQwMjk2NTJ8' \
+                      'NzFjMmJjMDk0YjAxMmE2ZjdmMzdlY2U0LjEtMTAwMDAyMzg3ODU0ODE1fElET0RNWXpNc3JqbklPLWsxbVVy' \
+                      'Qlk4LWpDayIsInVzZXIiOnsiY291bnRyeSI6Im5vIiwibG9jYWxlIjoiZW5fVVMiLCJhZ2UiOnsibWluIjoy' \
+                      'MX19LCJ1c2VyX2lkIjoiMTAwMDAyMzg3ODU0ODE1In0'
+                      
+TEST_APP_SECRET = '102d4e42d228d59c7ae4ebd874ef7757'
 
 def test_signed_request():
     graph = GraphAPI(signed_request=TEST_SIGNED_REQUEST, app_secret=TEST_APP_SECRET)
