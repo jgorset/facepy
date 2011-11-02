7f0157e297011558832b58668402e7c78f9f4b0e
Johannes Gorset <jgorset@gmail.com>
2011-11-02 10:35:21 +0100

    Fix indent

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-  name = 'facepy',
-  version = '0.4.0',
-  description = "Facepy is an API client for Facebook's Graph API that doesn't suck.",
-  author = "Johannes Gorset",
-  author_email = "jgorset@gmail.com",
-  url = "http://github.com/jgorset/facepy",
-  packages = ['facepy'],
-  install_requires = ['requests']
+    name = 'facepy',
+    version = '0.4.0',
+    description = "Facepy is an API client for Facebook's Graph API that doesn't suck.",
+    author = "Johannes Gorset",
+    author_email = "jgorset@gmail.com",
+    url = "http://github.com/jgorset/facepy",
+    packages = ['facepy'],
+    install_requires = ['requests']
 )
