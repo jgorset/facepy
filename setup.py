9812bc7a18ec835427227e9fcc5c10059086ae19
Johannes Gorset <jgorset@gmail.com>
2011-11-02 10:36:08 +0100

    Update setup description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'facepy',
     version = '0.4.0',
-    description = "Facepy is an API client for Facebook's Graph API that doesn't suck.",
+    description = "Facepy is a client for Facebook APIs.",
     author = "Johannes Gorset",
     author_email = "jgorset@gmail.com",
     url = "http://github.com/jgorset/facepy",
