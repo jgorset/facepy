e0caa86c23809ee28dbaa72054bbf2bb7d278dfe
Johannes Gorset <jgorset@gmail.com>
2011-12-27 20:24:27 +0100

    Use single quotes

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name = 'facepy',
     version = '0.5.1',
-    description = "Facepy is a client for Facebook APIs.",
-    author = "Johannes Gorset",
-    author_email = "jgorset@gmail.com",
-    url = "http://github.com/jgorset/facepy",
+    description = 'Facepy is a client for Facebook APIs.',
+    author = 'Johannes Gorset',
+    author_email = 'jgorset@gmail.com',
+    url = 'http://github.com/jgorset/facepy',
     packages = ['facepy'],
     install_requires = ['requests']
 )
