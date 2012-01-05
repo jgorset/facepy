c41472b05cc9fabd8e5e9bdd66a75d5b6eb70b09
Johannes Gorset <jgorset@gmail.com>
2012-01-05 16:03:26 +0100

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'facepy',
-    version = '0.5.1',
+    version = '0.6',
     description = 'Facepy makes it absurdly easy to interact with Facebook APIs',
     author = 'Johannes Gorset',
     author_email = 'jgorset@gmail.com',
