7d83df4c48baeb71fc14514444e256c632029eee
Johannes Gorset <jgorset@gmail.com>
2012-01-05 15:10:05 +0100

    Refactor tests

--- tests/test_graph_api.py
+++ tests/test_graph_api.py
@@ -4,60 +4,52 @@ import random
 
 from facepy import GraphAPI
 
-TEST_APP_ID = '160327664029652'
-TEST_APP_SECRET = '102d4e42d228d59c7ae4ebd874ef7757'
-TEST_USER = None
+TEST_APPLICATION_ID = '160327664029652'
+TEST_APPLICATION_SECRET = '102d4e42d228d59c7ae4ebd874ef7757'
 
 def setup_module(module):
-    setattr(module, 'TEST_USER', FacebookUser.create())
+    """
+    Create a Facebook test user.
+    """
+    global TEST_USER_ID
+    global TEST_USER_ACCESS_TOKEN
+
+    graph = GraphAPI('%s|%s' % (TEST_APPLICATION_ID, TEST_APPLICATION_SECRET))
+
+    user = graph.post('%s/accounts/test-users' % TEST_APPLICATION_ID,
+        installed = 'true',
+        permissions = 'publish_stream, read_stream'
+    )
+
+    TEST_USER_ID = user['id']
+    TEST_USER_ACCESS_TOKEN = user['access_token']
 
 def teardown_module(module):
-    if TEST_USER:
-        TEST_USER.delete()
-
-class FacebookUser(object):
-
-    def __init__(self, user_id, access_token):
-        self.user_id = user_id
-        self.access_token = access_token
-        self.graph = GraphAPI(self.access_token)
-
-    def delete(self):
-        application_graph = GraphAPI('%s|%s' % (TEST_APP_ID, TEST_APP_SECRET))
-        application_graph.delete("%s/" % self.user_id)
-
-    @classmethod
-    def create(cls):
-        application_graph = GraphAPI('%s|%s' % (TEST_APP_ID, TEST_APP_SECRET))
-
-        user = application_graph.post("%s/accounts/test-users" % TEST_APP_ID,
-            installed='true',
-            permissions='publish_stream,read_stream',
-        )
-
-        return cls(user['id'], user['access_token'])
+    """
+    Delete the Facebook test user.
+    """
+    GraphAPI('%s|%s' % (TEST_APPLICATION_ID, TEST_APPLICATION_SECRET)).delete(
+        path = TEST_USER_ID
+    )
 
 def test_get():
-    graph = TEST_USER.graph
+    graph = GraphAPI(TEST_USER_ACCESS_TOKEN)
 
-    assert isinstance(graph.get('me'), dict)
-    assert isinstance(graph.get('me/picture'), str)
+    # Test a simple get
+    assert graph.get('me')
 
-def test_paged_get():
-    graph = TEST_USER.graph
+    # Test a paged get
+    pages = graph.get('facebook/posts', page=True)
 
-    posts = graph.get('facebook/posts', page=True)
+    for index, page in enumerate(pages):
+        if index == 3:
+            break
 
-    for index, post in enumerate(posts):
-        if index > 3:
-            return
-
-    assert False, 'only first page of results returned'
+    assert index == 3
 
 def test_post():
-    graph = TEST_USER.graph
+    graph = GraphAPI(TEST_USER_ACCESS_TOKEN)
 
-    # Generate a random message (Facebook rejects duplicate messages within a short time frame)
     message = ''.join(random.sample('a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(), 10))
 
     response = graph.post(
@@ -70,24 +62,31 @@ def test_post():
     assert post['message'] == message
 
 def test_delete():
-    graph = TEST_USER.graph
-
-    # Generate a random message (Facebook rejects duplicate messages within a short time frame)
-    message = ''.join(random.sample('a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(), 10))
+    graph = GraphAPI(TEST_USER_ACCESS_TOKEN)
 
     response = graph.post(
         path = 'me/feed',
-        message = message
+        message = ''.join(random.sample('a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(), 10))
     )
 
-    assert graph.delete(response['id']) is True
+    assert graph.delete(
+        path = response['id']
+    )
 
 def test_search():
-    graph = GraphAPI()
+    graph = GraphAPI(TEST_USER_ACCESS_TOKEN)
 
-    results = graph.search(
+    # Test a simple search
+    assert graph.search(
         term = 'the meaning of life',
         type = 'post'
     )
 
-    assert isinstance(results['data'], list)
+    # Test a paged search
+    pages = graph.get('facebook/posts', page=True)
+
+    for index, page in enumerate(pages):
+        if index == 3:
+            break
+
+    assert index == 3
--- tests/test_signed_request.py
+++ tests/test_signed_request.py
@@ -1,5 +1,9 @@
 """Tests for the ``signed_request`` module."""
 
+from datetime import datetime
+
+from facepy import SignedRequest
+
 TEST_ACCESS_TOKEN = '181259711925270|1570a553ad6605705d1b7a5f.1-499729129|8XqMRhCWDKtpG-i_zRkHBDSsqqk'
 
 TEST_SIGNED_REQUEST = u'mnrG8Wc9CH_rh-GCqq97GFAPOh6AY7cMO8IYVKb6Pa4.eyJhbGdvcml0aG0iOi' \
@@ -11,11 +15,26 @@ TEST_SIGNED_REQUEST = u'mnrG8Wc9CH_rh-GCqq97GFAPOh6AY7cMO8IYVKb6Pa4.eyJhbGdvcml0
 
 TEST_FACEBOOK_APPLICATION_SECRET_KEY = '214e4cb484c28c35f18a70a3d735999b'
 
-def test_parse_signed_request():
-    from facepy import SignedRequest
-    from datetime import datetime
+def test_construct_signed_request():
+    assert SignedRequest(
+        user = SignedRequest.User(
+            id = 1,
+            age = range(0, 100),
+            locale = 'en_US',
+            country = 'US'
+        ),
+        oauth_token = SignedRequest.OAuthToken(
+            token = 'AAAAAAITEghMBAFjv7aoQrdnxDkYyNwpwGXSZBvoWH57Q0f...',
+            issued_at = datetime.now(),
+            expires_at = None
+        )
+    )
 
-    signed_request = SignedRequest.parse(TEST_SIGNED_REQUEST, TEST_FACEBOOK_APPLICATION_SECRET_KEY)
+def test_parse_signed_request():
+    signed_request = SignedRequest.parse(
+        signed_request = TEST_SIGNED_REQUEST,
+        application_secret_key = TEST_FACEBOOK_APPLICATION_SECRET_KEY
+    )
 
     assert signed_request.user.id == '499729129'
     assert signed_request.oauth_token.token == TEST_ACCESS_TOKEN
@@ -23,42 +42,42 @@ def test_parse_signed_request():
     assert signed_request.oauth_token.issued_at == datetime(2011, 5, 23, 21, 45, 4)
 
 def test_generate_signed_request():
-    from facepy import SignedRequest
-    from datetime import datetime
-
-    parsed_signed_request = SignedRequest.parse(TEST_SIGNED_REQUEST, TEST_FACEBOOK_APPLICATION_SECRET_KEY)
-
-    assert parsed_signed_request.oauth_token.token == '181259711925270|1570a553ad6605705d1b7a5f.1-499729129|8XqMRhCWDKtpG-i_zRkHBDSsqqk'
-    assert parsed_signed_request.oauth_token.expires_at == None
-    assert parsed_signed_request.oauth_token.issued_at == datetime(2011, 5, 23, 21, 45, 4)
-    assert parsed_signed_request.user.locale == 'en_US'
-    assert parsed_signed_request.user.country == 'no'
-    assert parsed_signed_request.user.age == range(21, 100)
-    assert parsed_signed_request.user.id == '499729129'
-
-    reverse_engineered_signed_request = parsed_signed_request.generate(TEST_FACEBOOK_APPLICATION_SECRET_KEY)
-
-    assert reverse_engineered_signed_request == '_BI1k2IFKMlIqbUdtr85034LlIZxMu7iS1xF-K8pYkE=.' \
-                                                'eyJ1c2VyX2lkIjoiNDk5NzI5MTI5IiwiYWxnb3JpdGhtI' \
-                                                'joiSE1BQy1TSEEyNTYiLCJleHBpcmVzIjowLCJvYXV0aF' \
-                                                '90b2tlbiI6IjE4MTI1OTcxMTkyNTI3MHwxNTcwYTU1M2F' \
-                                                'kNjYwNTcwNWQxYjdhNWYuMS00OTk3MjkxMjl8OFhxTVJo' \
-                                                'Q1dES3RwRy1pX3pSa0hCRFNzcXFrIiwidXNlciI6eyJsb' \
-                                                '2NhbGUiOiJlbl9VUyIsImNvdW50cnkiOiJubyIsImFnZS' \
-                                                'I6eyJtYXgiOjk5LCJtaW4iOjIxfX0sImlzc3VlZF9hdCI' \
-                                                '6MTMwNjE3OTkwNH0='
-
-    parsed_reverse_engineered_signed_request = SignedRequest.parse(
-        signed_request = reverse_engineered_signed_request,
+    signed_request = SignedRequest.parse(
+        signed_request = TEST_SIGNED_REQUEST,
         application_secret_key = TEST_FACEBOOK_APPLICATION_SECRET_KEY
     )
 
-    assert parsed_reverse_engineered_signed_request.oauth_token.token == '181259711925270|1570a553ad6605705d1b7a5f.1-499729129|8XqMRhCWDKtpG-i_zRkHBDSsqqk'
-    assert parsed_reverse_engineered_signed_request.oauth_token.expires_at == None
-    assert parsed_reverse_engineered_signed_request.oauth_token.has_expired == False
-    assert parsed_reverse_engineered_signed_request.oauth_token.issued_at == datetime(2011, 5, 23, 21, 45, 4)
-    assert parsed_reverse_engineered_signed_request.user.locale == 'en_US'
-    assert parsed_reverse_engineered_signed_request.user.country == 'no'
-    assert parsed_reverse_engineered_signed_request.user.age == range(21, 100)
-    assert parsed_reverse_engineered_signed_request.user.id == '499729129'
-    assert parsed_reverse_engineered_signed_request.user.has_authorized_application == True
+    assert signed_request.oauth_token.token == '181259711925270|1570a553ad6605705d1b7a5f.1-499729129|8XqMRhCWDKtpG-i_zRkHBDSsqqk'
+    assert signed_request.oauth_token.expires_at == None
+    assert signed_request.oauth_token.issued_at == datetime(2011, 5, 23, 21, 45, 4)
+    assert signed_request.user.locale == 'en_US'
+    assert signed_request.user.country == 'no'
+    assert signed_request.user.age == range(21, 100)
+    assert signed_request.user.id == '499729129'
+
+    signed_request = signed_request.generate(TEST_FACEBOOK_APPLICATION_SECRET_KEY)
+
+    assert signed_request == '_BI1k2IFKMlIqbUdtr85034LlIZxMu7iS1xF-K8pYkE=.' \
+                             'eyJ1c2VyX2lkIjoiNDk5NzI5MTI5IiwiYWxnb3JpdGhtI' \
+                             'joiSE1BQy1TSEEyNTYiLCJleHBpcmVzIjowLCJvYXV0aF' \
+                             '90b2tlbiI6IjE4MTI1OTcxMTkyNTI3MHwxNTcwYTU1M2F' \
+                             'kNjYwNTcwNWQxYjdhNWYuMS00OTk3MjkxMjl8OFhxTVJo' \
+                             'Q1dES3RwRy1pX3pSa0hCRFNzcXFrIiwidXNlciI6eyJsb' \
+                             '2NhbGUiOiJlbl9VUyIsImNvdW50cnkiOiJubyIsImFnZS' \
+                             'I6eyJtYXgiOjk5LCJtaW4iOjIxfX0sImlzc3VlZF9hdCI' \
+                             '6MTMwNjE3OTkwNH0='
+
+    signed_request = SignedRequest.parse(
+        signed_request = signed_request,
+        application_secret_key = TEST_FACEBOOK_APPLICATION_SECRET_KEY
+    )
+
+    assert signed_request.oauth_token.token == '181259711925270|1570a553ad6605705d1b7a5f.1-499729129|8XqMRhCWDKtpG-i_zRkHBDSsqqk'
+    assert signed_request.oauth_token.expires_at == None
+    assert signed_request.oauth_token.has_expired == False
+    assert signed_request.oauth_token.issued_at == datetime(2011, 5, 23, 21, 45, 4)
+    assert signed_request.user.locale == 'en_US'
+    assert signed_request.user.country == 'no'
+    assert signed_request.user.age == range(21, 100)
+    assert signed_request.user.id == '499729129'
+    assert signed_request.user.has_authorized_application == True
