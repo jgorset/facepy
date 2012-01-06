2b0d636f7b2ba2b9a2e0db3ca8ab2e946ccbf3a8
Johannes Gorset <jgorset@gmail.com>
2012-01-06 14:36:47 +0100

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('HISTORY.rst').read()
 
 setup(
     name = 'facepy',
-    version = '0.6',
+    version = '0.6.1',
     description = 'Facepy makes it absurdly easy to interact with Facebook APIs',
     long_description = readme + '\n\n' + history,
     author = 'Johannes Gorset',
