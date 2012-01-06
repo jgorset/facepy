57bf9d8e27c66b7ac76c8fa62e1a286ff3446c1e
Johannes Gorset <jgorset@gmail.com>
2012-01-06 14:27:22 +0100

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -8,5 +8,14 @@ setup(
     author_email = 'jgorset@gmail.com',
     url = 'http://github.com/jgorset/facepy',
     packages = ['facepy'],
-    install_requires = ['requests==0.8.3']
+    install_requires = ['requests==0.8.3'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
