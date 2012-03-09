c9ba9900af30aedd3d30af5f7d40b5dbc5b6c9ec
Johannes Gorset <jgorset@gmail.com>
2012-03-09 21:35:47 +0100

    Update requests

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email = 'jgorset@gmail.com',
     url = 'http://github.com/jgorset/facepy',
     packages = ['facepy'],
-    install_requires = ['requests >=0.8, < 0.9'],
+    install_requires = ['requests >=0.8, < 0.11'],
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
