269474608221e35907896f5f618e69d6e5136388
Johannes Gorset <jgorset@gmail.com>
2012-06-16 00:52:23 +0200

    Remove uneccessary getter and setter

--- facepy/exceptions.py
+++ facepy/exceptions.py
@@ -3,11 +3,3 @@ class FacepyError(Exception):
 
     def __init__(self, message):
         self.message = message
-
-    def _get_message(self):
-        return self._message
-
-    def _set_message(self, message):
-        self._message = message
-
-    message = property(_get_message, _set_message)
