4fd971315e8c6a5c72f4df019c06a017889123fb
J. Javier Maestro <jjmaestro@twoapart.com>
2012-08-23 18:51:11 +0200

    Fix PEP8 compliance

--- facepy/exceptions.py
+++ facepy/exceptions.py
@@ -1,21 +1,23 @@
 class FacepyError(Exception):
     """Base class for exceptions raised by Facepy."""
-
     def __init__(self, message):
         self.message = message
 
+
 class FacebookError(FacepyError):
     """Exception for Facebook errors."""
-
     def __init__(self, message=None, code=None):
         self.message = message
         self.code = code
 
+
 class OAuthError(FacebookError):
     """Exception for Facebook errors specifically related to OAuth."""
 
+
 class HTTPError(FacepyError):
     """Exception for transport errors."""
+
 
 class SignedRequestError(FacepyError):
     """Exception for invalid signed requests."""
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,10 +1,11 @@
 """Tests for the ``exceptions`` module."""
 
+import cPickle
+
 from nose.tools import *
 
 from facepy import *
-from facepy.graph_api import GraphAPI
-import cPickle
+
 
 def test_facepy_error():
     try:
@@ -12,17 +13,20 @@ def test_facepy_error():
     except FacepyError as exception:
         assert exception.message == '<message>'
 
+
 def test_facebookerror_can_be_pickled():
     try:
         raise GraphAPI.FacebookError('<message>', '<code>')
     except FacepyError as exception:
         cPickle.dumps(exception)
 
+
 def test_oautherror_can_be_pickled():
     try:
         raise GraphAPI.OAuthError('<message>', '<code>')
     except FacepyError as exception:
         cPickle.dumps(exception)
+
 
 def test_httperror_can_be_pickled():
     try:
