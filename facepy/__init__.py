14a70104a4a3ebc6bd333038e8277b308572cbbe
Simon Weber <simon@simonmweber.com>
2014-06-17 10:49:28 -0400

    expose exceptions at package level

--- facepy/__init__.py
+++ facepy/__init__.py
@@ -1,12 +1,22 @@
-from facepy.exceptions import FacepyError
+from facepy.exceptions import (
+    FacebookError,
+    FacepyError,
+    HTTPError,
+    OAuthError,
+    SignedRequestError
+)
 from facepy.graph_api import GraphAPI
 from facepy.signed_request import SignedRequest
 from facepy.utils import get_application_access_token, get_extended_access_token
 
 __all__ = [
     'FacepyError',
+    'FacebookError',
     'GraphAPI',
+    'HTTPError',
+    'OAuthError',
     'SignedRequest',
+    'SignedRequestError',
     'get_application_access_token',
     'get_extended_access_token',
 ]
