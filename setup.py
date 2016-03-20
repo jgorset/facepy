27e55ab537627da0bf595144c9d6b842682374f1
Marco Badan <info@marcobadan.com>
2016-03-20 12:00:56 +0100

    Add Python 3.5 support
    *  Update tox, travis configuration
    * Add python 3 verions to setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     zip_safe=False
 )
